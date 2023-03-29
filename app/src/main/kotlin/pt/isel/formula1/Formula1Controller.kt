44e3fd6bca4a2b6c92cec126316b9014597d1a1d
diogorodrigues <a49513@alunos.isel.pt>
2023-03-29 17:31:42 +0100

    Formula1Controller map completed

--- app/src/main/kotlin/pt/isel/formula1/Formula1Controller.kt
+++ app/src/main/kotlin/pt/isel/formula1/Formula1Controller.kt
@@ -10,35 +10,36 @@ import java.util.*
 class Formula1Controller {
     val repo = mutableMapOf(
         "RedBull" to listOf(
-            Driver(1, "Max Verstappen", 340000.50, true,  1),
+            Driver(1, "Max Verstappen", 340000.50, true, 1),
             Driver(2, "Pierre Gasly", 540000.25, false, null),
             Driver(3, "Alexander Albon", 280000.89, true, 4),
         ),
         "Mercedes" to listOf(
-            Driver(4, "Lewis Hamilton", 6700000, false, null),
-            Driver(5, "Valtteri Bottas", "31M", 2),
-            Driver(6, "George Russell", "22M", 3),
+            Driver(4, "Lewis Hamilton", 6700000.78, false, null),
+            Driver(5, "Valtteri Bottas", 902345.88, true, 3),
+            Driver(6, "George Russell", 140789.89, false, null),
         ),
         "Ferrari" to listOf(
-            Driver(7, "Charles Leclerc", "15F", 1),
-            Driver(8, "Sebastian Vettel", "55F", 2),
-            Driver(9, "Carlos Sainz", "67F", 3),
+            Driver(7, "Charles Leclerc", 654792.45, true, 6),
+            Driver(8, "Sebastian Vettel", 789675.45, false, null),
+            Driver(9, "Carlos Sainz", 126789.12, true, 2),
         ),
     )
 
     /**
      * Example:
-     *   http://localhost:4000/teams/Ferrari?driver=Max
+     *   http://localhost:4000/teams/Ferrari?driver=Max&active=true
      */
     @Synchronized
     @AutoRouter("/teams/{teamName}")
-    fun search(
+    fun searchDriver(
         @ArRoute teamName: String,
         @ArQuery driver: String?,
+        @ArQuery active: Boolean? ,
     ): Optional<List<Driver>> {
         return repo[teamName]
             ?.let {
-                if (driver == null) {
+                if (driver == null && active == null) {
                     Optional.of(it)
                 } else {
                     Optional.of(it.filter { dr -> dr.name.lowercase().contains(driver.lowercase()) })
@@ -73,7 +74,10 @@ class Formula1Controller {
      */
     @Synchronized
     @AutoRouter("/teams/{teamName}/drivers/{driverId}", method = ArVerb.DELETE)
-    fun removeDriver(@ArRoute teamName: String, @ArRoute driverId: Int): Optional<Driver> {
+    fun removeDriver(
+        @ArRoute teamName: String,
+        @ArRoute driverId: Int
+    ): Optional<Driver> {
         val stds = repo[teamName] ?: return Optional.empty()
         val s = stds.firstOrNull { it.driverId == driverId } ?: return Optional.empty()
         repo[teamName] = stds.filter { it.driverId != driverId }
