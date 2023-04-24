ebd3ac46fca1583d82fdf7a7f14495fbe8b4b77a
FranciscoEngenheiro <a49428@alunos.isel.pt>
2023-04-24 10:34:21 +0100

    First buildHandler implementation.

--- autorouter/src/main/java/pt/isel/autorouter/AutoRouterDynamic.java
+++ autorouter/src/main/java/pt/isel/autorouter/AutoRouterDynamic.java
@@ -7,18 +7,13 @@ import org.cojen.maker.Variable;
 import pt.isel.autorouter.annotations.ArBody;
 import pt.isel.autorouter.annotations.ArQuery;
 import pt.isel.autorouter.annotations.ArRoute;
-import pt.isel.autorouter.exceptions.ArTypeAnnotationNotFoundException;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Stream;
 
-
-import static com.fasterxml.jackson.databind.type.LogicalType.Map;
-
 public class AutoRouterDynamic {
+
     public static Stream<ArHttpRoute> autorouterDynamic(Object controller) {
         return Stream.empty();
     }
@@ -41,34 +36,43 @@ public class AutoRouterDynamic {
         //Inicia o field router do constructor  this.router = router;
         ctor.field(routerMaker.name()).set(ctor.param(0));
 
-        //Criacao da assintura do metoodo(handler)
+        //Criacao da assintura do metodo(handler)
         MethodMaker handlerMaker = clazzMaker.addMethod(Optional.class, "handler", Map.class, Map.class, Map.class)
                 .public_()
                 .override();
 
+        Map<String, Variable> mapArgs = new HashMap<>();
         // @ARroute routeargs  @ArQuery queryargs @Arbody bodyargs
         Stream<Variable> mapa = Arrays //search 2 parameters /add 2 parmeters
-                .stream(fun.getParameters()).map(
-                        //Todo: usar uma mapa para assiciar o parametro.getname() ao mapa que queremos
-                        param -> {
-                           //param.getName()
-                            if (param.isAnnotationPresent(ArRoute.class)) {
-                                return handlerMaker.param(0); //associacao do mapa ao parametro
-                            } else if (param.isAnnotationPresent(ArQuery.class)) {
-                                return handlerMaker.param(1);
-                            } else if (param.isAnnotationPresent(ArBody.class)) {
-                                return handlerMaker.param(2);
-                            }
-                            try {
-                                throw new ArTypeAnnotationNotFoundException(
-                                        "Ar type annotation was not found in the " + param.getName() + " parameter");
-                            } catch (ArTypeAnnotationNotFoundException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                );
-        //TODO(Buscar o get com o reflect ou pelo menos tentar )
-        return null;
+            .stream(fun.getParameters()).map(
+                //Todo: usar uma mapa para assiciar o parametro.getname() ao mapa que queremos
+                param -> {
+                   //param.getName()
+                    if (param.isAnnotationPresent(ArRoute.class)) {
+                        mapArgs.put(param.getName(), handlerMaker.param(0));
+                        return handlerMaker.param(0);
+                    } else if (param.isAnnotationPresent(ArQuery.class)) {
+                        mapArgs.put(param.getName(), handlerMaker.param(1));
+                        return handlerMaker.param(1);
+                    } else if (param.isAnnotationPresent(ArBody.class)) {
+                        mapArgs.put(param.getName(), handlerMaker.param(2));
+                        return handlerMaker.param(2);
+                    }
+                    return null;
+                }
+            );
+        // classroom -> routeargs
+        // String Classroom = routeArgs.get("classroom");?
+        // TODO(Buscar o get com o reflect ou pelo menos tentar)
+        ArrayList<Object> args = new ArrayList<>();
+        for (Map.Entry<String, Variable> entry : mapArgs.entrySet()) {
+            String key = entry.getKey();
+            Variable value = entry.getValue();
+            // routeArgs.get("classroom");
+            args.add(value.invoke("get", key));
+        }
+        handlerMaker.field(routerMaker.name()).invoke(fun.getName(), args.toArray());
+        return clazzMaker;
     }
 
     /*public class buildHttpHandlerSearch implements ArHttpHandler {
@@ -80,9 +84,11 @@ public class AutoRouterDynamic {
 
     @Override
     public Optional<?> handle(Map<String, String> routeArgs, Map<String, String> queryArgs, Map<String, String> bodyArgs) {
+        // for -> args
         String Classroom = routeArgs.get("classroom");
         String Student = queryArgs.get("student");
-        return router.search(Classroom, Student);
+
+        return router.search(args[0], args[1]);
     }
 }
 
