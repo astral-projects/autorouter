f063279ed5208e5762c6a3f6639774b4974d4d77
FranciscoEngenheiro <a49428@alunos.isel.pt>
2023-05-24 22:17:03 +0100

    Added another test to WatchNewFilesContentTest.

--- watch-new-files-content/src/test/kotlin/WatchNewFilesContentTest.kt
+++ watch-new-files-content/src/test/kotlin/WatchNewFilesContentTest.kt
@@ -1,48 +1,49 @@
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.TestInstance.Lifecycle
-import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.nio.file.attribute.FileAttribute
-import java.util.concurrent.ConcurrentMap
 import java.util.concurrent.CountDownLatch
-import java.util.concurrent.Semaphore
 import kotlin.concurrent.thread
-import kotlin.io.path.createFile
 import kotlin.io.path.writeText
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class WatchNewFilesContentTest {
 
-    private val defaultText = "testString"
-    private val fileAName = "A.txt"
-    private val fileBName = "B.txt"
+    private val defaultTextA = "testStringA"
+    private val defaultTextB = "testStringB"
 
     private lateinit var dir: Path
     private lateinit var fileA: Path
     private lateinit var fileB: Path
+    private lateinit var students: Path
 
     @BeforeEach
     fun setup() {
         val directoryName = "watchable-dir"
         dir = Files.createDirectory(Paths.get("src/test/kotlin/$directoryName"))
-        fileA = Files.createFile(dir.resolve(fileAName))
-        fileB = Files.createFile(dir.resolve(fileBName))
+        fileA = Files.createFile(dir.resolve("A.txt"))
+        fileB = Files.createFile(dir.resolve("B.txt"))
+        students = Files.createFile(dir.resolve("students.txt"))
+        students.writeText(
+            List(10) {
+                "Student $it"
+            }.toList().joinToString("\n")
+        )
     }
 
     @AfterEach
     fun dispose() {
         Files.deleteIfExists(fileA)
         Files.deleteIfExists(fileB)
+        Files.deleteIfExists(students)
         Files.deleteIfExists(dir)
     }
 
     // try-out-tests
-    @Test
+    // @Test
     fun `Watch a directory lazily`() {
-        val dir = Paths.get("src/test/kotlin/watchable-dir")
         for (fileContent in dir.watchNewFilesContent()) {
             for (line in fileContent) {
                 println(line)
@@ -52,9 +53,7 @@ class WatchNewFilesContentTest {
 
     @Test
     fun `Watch a directory eagerly`() {
-        val dir = Paths.get("src/test/kotlin/watchable-dir")
-        val child: Path = dir.resolve("students.txt")
-        Files.newBufferedReader(child).use { reader ->
+        Files.newBufferedReader(students).use { reader ->
             // next() and hasNext() logic here
             while (true) {
                 val line = reader.readLine() ?: break
@@ -65,7 +64,7 @@ class WatchNewFilesContentTest {
 
     // concurrent-tests
     @Test
-    fun `step-by-step`() {
+    fun `Watch a directory lazily concurrently with one file modified only`() {
         val sequence= dir.watchNewFilesContent()
         val latch = CountDownLatch(1)
         lateinit var lines: Sequence<String>
@@ -77,14 +76,58 @@ class WatchNewFilesContentTest {
         }
         // wait for threadWatchFile start watching
         latch.await()
-        Thread.sleep(2000) // ensure threadWatchFile is watching
+        // ensure
+        Thread.sleep(2000)
         // write to file
-        fileA.writeText(defaultText)
+        fileA.writeText(defaultTextA)
         // wait for new file has been watched by threadWatchFile
         println("Waiting for threadWatchFile to finish")
         threadWatchFile.join()
         val iter = lines.iterator()
         assertTrue { iter.hasNext() }
-        assertEquals(defaultText, iter.next())
+        assertEquals(defaultTextA, iter.next())
+    }
+
+    @Test
+    fun `Watch a directory lazily concurrently with more than one file modified`() {
+        val sequence= dir.watchNewFilesContent()
+        val latch = CountDownLatch(1)
+        lateinit var linesA: Sequence<String>
+        val threadAWatchFile = thread {
+            // signal that threadWatchFile is ready to watch
+            latch.countDown()
+            // start watching and blocking until new file created or modified
+            linesA = sequence.iterator().next()
+        }
+        // wait for threadWatchFile start watching
+        latch.await()
+        // ensure
+        Thread.sleep(2000)
+        // write to file
+        fileA.writeText(defaultTextA)
+        threadAWatchFile.join()
+        val latchB = CountDownLatch(1)
+        lateinit var linesB: Sequence<String>
+        val threadBWatchFile = thread {
+            // signal that threadWatchFile is ready to watch
+            latchB.countDown()
+            // start watching and blocking until new file created or modified
+            linesB = sequence.iterator().next()
+        }
+        latchB.await()
+        Thread.sleep(2000)
+        // write to file
+        fileB.writeText(defaultTextB)
+        threadBWatchFile.join()
+        // A-Assertions
+        val iterA = linesA.iterator()
+        assertTrue { iterA.hasNext() }
+        assertEquals(defaultTextA, iterA.next())
+        assertFalse { iterA.hasNext() }
+        // B-Assertions
+        val iterB = linesB.iterator()
+        assertTrue { iterB.hasNext() }
+        assertEquals(defaultTextB, iterB.next())
+        assertFalse { iterB.hasNext() }
     }
 }
